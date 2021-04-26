6f59d7bc1b2e5102e73ff0e33ab72048fafb95ac
Константин Рощин <kvroschin@miem.hse.ru>
2021-04-26 16:11:54 +0300

    Tried to make blocksize=0 setting

--- FFT.py
+++ FFT.py
@@ -1,9 +1,12 @@
 import scipy.fftpack as fft
 import numpy as np
 import matplotlib.pyplot as plt
+from commpy.filters import rcosfilter as rc
 
 
 def format_figure(blocksize, samplerate, data):
+    scale = 4
+
     # create matplotlib figure and axes
     fig, (ax1, ax2) = plt.subplots(2, figsize=(15, 7))
 
@@ -21,23 +24,34 @@ def format_figure(blocksize, samplerate, data):
     ax1.set_title('AUDIO WAVEFORM')
     ax1.set_xlabel('samples')
     ax1.set_ylabel('volume')
-    ax1.set_ylim(-1, 1)
+    ax1.set_ylim(-1 / scale, 1 / scale)
     ax1.set_xlim(0, blocksize)
-    plt.setp(ax1, xticks=[0, blocksize], yticks=np.linspace(-1, 1, num=5, endpoint=True))
+    plt.setp(ax1, xticks=[0, blocksize], yticks=np.linspace(-1 / scale, 1 / scale, num=5, endpoint=True))
 
     # format spectrum axes
     ax2.set_xlim(20, samplerate / 2)
+    ax2.set_ylim(0, 0.2 / scale)
+
+    plt.show(block=False)
 
     return fig, line, line_fft
 
 
+def interpolate(data, n):
+
+    interdata = np.zeros(len(data) * n)
+    interdata[:: n] = data
+
+    return
+
+
 def update(fig, line, line_fft, data, blocksize):
 
-    line.set_ydata(data)
+    if blocksize != 0:
+        line.set_ydata(data)
 
-    yf = fft.fft(data)
-    line_fft.set_ydata(np.abs(yf[0:blocksize]) * 2 / blocksize)
+        yf = fft.fft(data)
+        line_fft.set_ydata(np.abs(yf[0:blocksize]) * 2 / blocksize)
 
-    fig.canvas.draw()
-    fig.canvas.flush_events()
-    plt.show(block=False)
+        fig.canvas.draw()
+        fig.canvas.flush_events()
