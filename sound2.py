4e7dc6fe22a5838f8b46e71b315cbf0c98a93cf6
kvroschin@miem.hse.ru <kvroschin@miem.hse.ru>
2021-04-27 18:08:48 +0300

    Tried using callback

--- sound2.py
+++ sound2.py
@@ -1,7 +1,27 @@
 import numpy as np
 import sounddevice as sd
+import FFT
 
 
+sd.default.latency = [0.0, 0.0]
 samplerate = sd.query_devices(sd.default.device, 'input')['default_samplerate']
 blocksize = 1024 * 2
-data = np.zeros([blocksize, 2])
+data = np.zeros([blocksize])
+fig, line, line_fft = FFT.format_figure(blocksize, samplerate, data)
+inbuf = np.zeros(blocksize)
+
+
+def callback(indata, frames, time, status):
+    global inbuf
+    shift = len(indata[:, 0])
+    inbuf = np.roll(inbuf, -shift)
+    inbuf[-shift:] = indata[:, 0]
+
+    FFT.update(fig, line, line_fft, inbuf, blocksize)
+
+
+stream = sd.InputStream(channels=1, callback=callback, latency=0.0)
+
+with stream as s:
+    while True:
+        sd.sleep(1000)
